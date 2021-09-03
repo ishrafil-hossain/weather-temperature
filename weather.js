7fc76eb45cc95842e4117ea5f86b0bc02fa098ac
Md Ishrafil Hossain <ishrafil2233@gmail.com>
2021-09-03 23:55:59 +0600

    update something

--- weather.js
+++ weather.js
@@ -4,18 +4,18 @@ const loadTemperature = () => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}&units=metric`;
     fetch(url)
         .then(res => res.json())
-        .then(data => displayTemperature(data))
-        .catch(err => errorMessage(err));
+        .then(data => displayTemperature(data));
+    // .catch(err => errorMessage());
 }
 
-const errorMessage = (err) => {
-    const error = document.getElementById('city');
-    error.style.color = 'red';
-    error.innerText = `Please write correct city name`;
-    const temp = document.getElementById('temp').style.display = 'none';
-    const image = document.getElementById('image').style.display = 'none';
-    const condition = document.getElementById('condition').style.display = 'none';
-}
+// const errorMessage = (err) => {
+//     const error = document.getElementById('city');
+//     error.style.color = 'red';
+//     error.innerText = `Please write correct city name`;
+//     const temp = document.getElementById('temp').style.display = 'none';
+//     const image = document.getElementById('image').style.display = 'none';
+//     const condition = document.getElementById('condition').style.display = 'none';
+// }
 
 const setText = (id, name) => {
     const city = document.getElementById(id).innerText = name;
